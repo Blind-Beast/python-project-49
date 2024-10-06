3b8ed39335eee7aa6a7f1591c5cd4dba7a4b2c96
blindbeast <slava5283@gmail.com>
2024-10-06 12:49:47 +0300

    edit prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,7 +1,7 @@
 from random import randint
 
 GAME_QUESTION = "Answer \'yes\' if given number is prime."\
-                " Otherwise answer \'no\'"
+                " Otherwise answer \'no\'."
 
 
 def is_prime(number):
